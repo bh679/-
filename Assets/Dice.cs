2bc2ca5a0981738cf95ccb1001e18db28f5289ac
Brennan Hatton <brennan@brennanhatton.com>
2023-03-05 17:58:26 +1100

    Adding dice

---
+++ Assets/Dice.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A UnityEvent with a Grabber as the parameter
+/// </summary>
+[System.Serializable]
+public class IntEvent : UnityEvent<int> { }
+    
+public class Dice : MonoBehaviour
+{
+	public IntEvent onLand, onUpdateDisplay;
+	
+	public GameObject[] images;
+	public int value;
+	
+	public Vector2 landTime = new Vector2(1f, 5f);
+	
+	public void Roll()
+	{
+		StartCoroutine(rolling());
+	}
+	
+	IEnumerator rolling()
+	{
+		float time = Random.Range(landTime.x, landTime.y);
+		
+		while(time > 0)
+		{
+			yield return new WaitForSeconds(0.1f);
+			
+			value = Random.Range(0,6);
+			UpdateDisplay(value);
+			
+			time -= 0.1f;
+		}
+		
+		value = Random.Range(0,6);
+		UpdateDisplay(value);
+		
+		onLand.Invoke(value);
+	}
+	
+	void UpdateDisplay(int val)
+	{
+		onUpdateDisplay.Invoke(val);
+		
+		for(int i = 0 ; i < images.Length; i++)
+		{
+			images[i].SetActive(false);
+		}
+		
+		images[val].SetActive(true);
+	}
+}
